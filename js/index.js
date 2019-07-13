adcf3200b8b7a244ed88f953b7a0748ec08185f0
bufgix <ooruc471@yandex.com>
2019-07-13 22:29:01 +0300

    design changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-// Remove the spinning animation after 500ms.
-setTimeout(function() {
-    $(".overlay").fadeOut("slow");
-}, 500);
+$(document).ready(function() {
+    AOS.init({
+        duration: 600
+    });
+
+    // Remove the spinning animation after 500ms.
+    setTimeout(function() {
+        $(".overlay").fadeOut("slow");
+    }, 500);
+
+
+})
